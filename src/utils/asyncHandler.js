c6f89327580a9e4faa14aec49052ac6742396d8b
Vivek Pathak <vivekpathak8198@gmail.com>
2024-04-16 22:29:55 +0530

    util code discussion

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,19 @@
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+// const asyncHandler = (fn) => async(req,res,next)=> {
+//   try {
+//     await fn(req,res,next)
+//   } catch (error) {
+//     res.status(error.code || 500).json({
+//       succuss:false,
+//       message:error.message
+//     })
+
+//   }
+// };
+
+export { asyncHandler };
