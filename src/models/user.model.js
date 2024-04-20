ed206be664f627e0936c30b9348064b34627f37c
Vivek Pathak <vivekpathak8198@gmail.com>
2024-04-20 13:28:06 +0530

    add registerUser and controller

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -4,7 +4,7 @@ import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
   {
-    username: {
+    userName: {
       type: String,
       required: true,
       unique: true,
@@ -53,39 +53,39 @@ const userSchema = new Schema(
 
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
-  this.password = bcrypt.hash(this.password, 10);
+  this.password = await bcrypt.hash(this.password, 10);
 
   next();
 });
 
-userSchema.methods.isPasswordCorrect = async function(){
-    return await bcrypt.compare(password,this.password)
-}
+userSchema.methods.isPasswordCorrect = async function () {
+  return await bcrypt.compare(password, this.password);
+};
 
-userSchema.methods.generateAccessToken() = function(){
-   return jwt.sign(
-    {
-    _id:this._id,
-    email:this.email,
-    username:this.username,
-    fullName:this.fullName
-  },
-  process.env.ACCESS_TOKEN_SECRET,
-  {
-    expiresIn:process.env.ACCESS_TOKEN_EXPIRY
-  }
-)
-}
-userSchema.methods.generateRefreshToken() = function(){
+userSchema.methods.generateAccessToken = function () {
   return jwt.sign(
     {
-    _id:this._id,
-  },
-  process.env.REFRESH_TOKEN_SECRET,
-  {
-    expiresIn:process.env.REFRESH_TOKEN_EXPIRY
-  }
-)
-}
+      _id: this._id,
+      email: this.email,
+      userName: this.userName,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
 
-export const user = mongoose.model("User", userSchema);
+export const User = mongoose.model("User", userSchema);
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -7,7 +7,7 @@ cloudinary.config({
   api_secret: "process.env.CLOUDINARY_API-SECRET",
 });
 
-const uploadCloudinary = async (localFilePath) => {
+const uploadOnCloudinary = async (localFilePath) => {
   try {
     if (!localFilePath) return null;
     //upload the file on cloudinary
@@ -23,4 +23,4 @@ const uploadCloudinary = async (localFilePath) => {
   }
 };
 
-export { uploadCloudinary };
+export { uploadOnCloudinary };
